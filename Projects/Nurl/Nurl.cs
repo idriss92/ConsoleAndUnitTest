e904648c476d32f868fc1078517c41f99330ae7e
idriss92 <idriss2004@hotmail.com>
2014-05-16 03:24:12 +0200

    Main

---
+++ Projects/Nurl/Nurl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nurl
+{
+    public class Nurl
+    {
+        static void Main(string[] args)
+        {
+            Commander consol = new Commander();
+            if (args.Length > 0)
+            {
+                string operateur = args[0];
+                switch(operateur)
+                {
+                    case "get" :
+                        
+                            if (args.Length == 3)
+                                consol.Get(args);
+                            else 
+                                consol.GetSave(args);
+                            break;
+                        
+
+                    case "test" :
+                        
+                            if(args.Length == 5)
+                                consol.LoadTime(args);
+                            else
+                                consol.LoadTimeAverage(args);
+                            break;
+                   
+                    default :
+                        Console.WriteLine("Erreur");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Aucun contenu dans le tableau argument");
+            }
+            //Console.ReadKey();
+        }
+    }
+}
