28b0fd67734172bcba0520fb2dbe5d1004e409b2
idriss92 <idriss2004@hotmail.com>
2014-05-16 03:00:16 +0200

    initial

---
+++ Projects/Nurl.Test/MesTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nurl;
+using NUnit.Framework;
+
+
+namespace Nurl.Test
+{
+    [TestFixture]
+    public class MesTests
+    {
+        [Test]
+        public void Should_show_the_content_of_a_page()
+        {
+            //var 
+            ////given
+            //var command = null; //votre implémentation
+
+            ////when
+            //var result = command.Show(url); //exemple d'implémentation
+
+            ////then
+
+            //Assert.That(result, Is.EqualTo("<h1>hello</h1>"));
+        }
+
+        [Test]
+        public void Should_save_the_content_of_a_page_on_disk()
+        {
+        }
+
+        [Test]
+        public void Should_test_the_load_of_the_content_of_a_page()
+        {
+        }
+
+        [Test]
+        public void Should_show_the_average_of_load_time_of_the_content_of_a_page()
+        {
+        }
+
+
+
+
+
+    }
+}
